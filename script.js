1e29b5bf24c92a9f6df9699e8250528499a7e4c1
Caio <caiosouzadejesus085@gmail.com>
2026-03-17 12:26:20 -0300

    Colocando JS no projeto

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 function toggleMode() {
   const html = document.documentElement;
-
-  /*if (html.classList.contains("light")) {
-    html.classList.remove("light");
-  } else {
-    html.classList.add("light");
-  }*/
-
+  // if(html.classList.contains("light")) html.classList.remove("light");
+  // else html.classList.add("light" );
   html.classList.toggle("light");
+
+  const img = document.querySelector("#profile img");
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/Avatar.png");
+    img.setAttribute(
+      "alt",
+      "Foto do aprendiz em programação Caio Souza, com roupa clara e sem oculos",
+    );
+  } else {
+    img.setAttribute("src", "./assets/avatar-dark.png");
+    img.setAttribute(
+      "alt",
+      "Foto do aprendiz em programação Caio Souza, cabelo ondulado, e com oculos",
+    );
+  }
 }
