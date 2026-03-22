b27574dd1df72422b39daa3cc807e5813b24f601
Caio <caiosouzadejesus085@gmail.com>
2026-03-21 21:03:12 -0300

    animation on transition button

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ function toggleMode() {
   const html = document.documentElement;
   // if(html.classList.contains("light")) html.classList.remove("light");
   // else html.classList.add("light" );
+  // modo simplificado:
   html.classList.toggle("light");
 
   const img = document.querySelector("#profile img");
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/Avatar.png");
+    img.setAttribute("src", "./assets/avatar-light.png");
     img.setAttribute(
       "alt",
       "Foto do aprendiz em programação Caio Souza, com roupa clara e sem oculos",
     );
   } else {
-    img.setAttribute("src", "./assets/avatar-dark.png");
+    img.setAttribute("src", "./assets/avatar.png");
     img.setAttribute(
       "alt",
       "Foto do aprendiz em programação Caio Souza, cabelo ondulado, e com oculos",
