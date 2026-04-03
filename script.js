4d4328cc5e79becc8a9aed3b5726684624f0442a
Caio <caiosouzadejesus085@gmail.com>
2026-04-03 14:21:35 -0300

    fix error on image-dark

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 function toggleMode() {
   const html = document.documentElement;
-  // if(html.classList.contains("light")) html.classList.remove("light");
-  // else html.classList.add("light" );
-  // modo simplificado:
+
   html.classList.toggle("light");
 
   const img = document.querySelector("#profile img");
+
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png");
-    img.setAttribute(
-      "alt",
-      "Foto do aprendiz em programação Caio Souza, com roupa clara e sem oculos",
-    );
+    // Modo Claro:
+    img.src = "./assets/avatar-light.png";
+    img.alt = "Foto do aprendiz em programação Caio Souza";
   } else {
-    img.setAttribute("src", "./assets/avatar.png");
-    img.setAttribute(
-      "alt",
-      "Foto do aprendiz em programação Caio Souza, cabelo ondulado, e com oculos",
-    );
+    // Modo Escuro:
+    img.src = "./assets/avatar-dark.png";
+    img.alt = "Foto do aprendiz em programação Caio Souza";
   }
 }
